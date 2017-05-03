24bf2f5bcd039ae17d31af6d78fd9ab99e08757c
tam20082008 <tam20082008@gmail.com>
2017-05-03 07:18:36 +0700

    Commit change

---
+++ app/src/main/java/com/example/nguyenhuuthe/b2github/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.nguyenhuuthe.b2github;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by NguyenHuuThe on 5/3/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
